b17ceef223110f3837dbb889765e6b83a5fd7aea
= <antoinegexfabry@gmail.com>
2018-12-02 10:14:01 +0100

    updated

--- day2/puzzle2.py
+++ day2/puzzle2.py
@@ -7,9 +7,10 @@
 
 import csv
 import os
-path = '/Users/antoinegex-fabry/Desktop/AdventOfCode2018/day2'
+path = 'your_path'
 os.chdir(path)
 
+# Read the data from the text file
 results = []
 i = 0
 with open('input.txt', newline = '') as inputfile:
@@ -20,10 +21,7 @@ with open('input.txt', newline = '') as inputfile:
         data[i] = ele[:-1]
         i += 1
 
-
-
-
-# example
+# example data
 # data = ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab']
 total2 = 0
 total3 = 0
@@ -35,7 +33,7 @@ for row in data:
     # Count the EXACT number 2 and EXACT 3
     two_letters = sum([ele == 2 for ele in counting])
     three_letters = sum([ele == 3 for ele in counting])
-
+    # Controlling for the specific conditions
     if two_letters >= 1 and three_letters >= 1:
         total2 += 1
         total3 += 1
@@ -46,9 +44,8 @@ for row in data:
         total2 += 0
         total3 += 1
 
+# Results:
 print(str(total2) + ' boxes contain a letter which appears exactly twice.')
 print(str(total3) + ' boxes contain a letter which appears exactly three times.')
 print('Out of ' + str(len(data)) + ' Box IDs.')
 print('RESULT: ' + str(total2 * total3))
-
-print( [True, False, False] == True )
